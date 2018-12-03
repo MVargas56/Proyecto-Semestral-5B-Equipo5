c8d292c9c3fb71a533164b762bd53acb501e744f
felipedjcc <felipedjjc@gmail.com>
2018-12-02 21:03:21 -0600

    avance de proyecto
    realizacion de barra de vida de personaje y enemigo todavia no funciona muy bien

---
+++ proyecto_final_fps_equipo5/Assets/scrips/BarraDeVida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraDeVida : MonoBehaviour {
+
+    public Scrollbar vida_UI;
+    float vida;
+	// Use this for initialization
+	void Start () {
+        vida = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        vida_UI.size = vida;
+
+        //if (vida)
+        {
+
+        }
+		
+	}
+}
