455abb4faf3d226c48cb6610f3b9f3a0c192c11c
MVargas56 <36772961+MVargas56@users.noreply.github.com>
2018-12-10 17:10:08 -0600

    Proyecto base
    -terreno de prueba
    -controlador fps frefabricado
    -arquitectura para enemigos
    -pistola con rango efectivo
    -canvas para la mira

---
+++ Semestral Shooter/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy1 : EnemyBase
+{
+    float valH;
+    float valV;
+
+    private void Start()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        FollowPlayer();
+
+        if (EnemyHealth <= 0)
+        {
+            Debug.Log("Enemy Destroyed");
+            Destroy(gameObject);
+        }
+    }
+
+    void DeductPoints(int DamageAmount)
+    {
+        EnemyHealth -= DamageAmount;
+        Debug.Log("Enemy Health = " + EnemyHealth);
+    }
+
+    void FollowPlayer()
+    {
+        if (playerObj != null)
+        {
+            valH = 0;
+            valV = 1;
+            float ditsToPlayer = Vector3.Distance(transform.position, playerObj.transform.position);
+
+            float currentRotation = transform.rotation.eulerAngles.y * Mathf.Deg2Rad;
+
+            float moveZ = valV * velocity.z * Time.deltaTime;
+            float moveX = valH * velocity.x * Time.deltaTime;
+
+            Vector3 pos = transform.position;
+            pos.z += moveZ * Mathf.Cos(currentRotation);
+            pos.x += moveZ * Mathf.Sin(currentRotation);
+            transform.position = pos;
+
+            transform.Rotate(new Vector3(0, moveX, 0));
+
+            transform.LookAt(playerObj.transform.position);
+            //enemy.destination = playerObj.transform.position;
+        }
+    }
+}
---
+++ Semestral Shooter/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBase : MonoBehaviour
+{
+    protected GameObject playerObj;
+    protected NavMeshAgent enemy;
+    protected Rigidbody erb;
+    protected int EnemyHealth = 10;
+    public Vector3 velocity;
+    
+    void Start ()
+    {
+
+    }
+	
+	void Update () {
+		
+	}
+}
